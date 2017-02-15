ed062787b7c6988ba7d74971c40b7245bca1e86b
Marco Bruno <marco.bruno.br@gmail.com>
2017-02-14 22:01:09 -0200

    [Ex. 3.7] - Alterando visualização dos cartões com JS

---
+++ javascript/block/cabecalho.js
@@ -0,0 +1,13 @@
+var $mudaLayout = document.querySelector('#mudaLayout')
+var $mural = document.querySelector('.mural')
+
+$mudaLayout.addEventListener('click', function() {
+
+  $mural.classList.toggle('mural--linhas')
+
+  if ($mural.classList.contains('mural--linhas')) {
+    $mudaLayout.textContent = 'Colunas'
+  } else {
+    $mudaLayout.textContent = 'Linhas'
+  }
+})

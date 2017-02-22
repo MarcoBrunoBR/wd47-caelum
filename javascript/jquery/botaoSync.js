3f47f3a3692f57907078e0d786fb878e3de2e9aa
Marco Bruno <marco.bruno.br@gmail.com>
2017-02-22 19:25:57 -0300

    [Adicional] - Colando o botaoSync em uma IIFE

--- javascript/jquery/botaoSync.js
+++ javascript/jquery/botaoSync.js
@@ -1,39 +1,41 @@
-let $sync = $('#sync')
+(function () {
+  let $sync = $('#sync')
 
-$sync.click(function () {
-  let $cartoes = $('.cartao')
-  let cartoes = []
+  $sync.click(function () {
+    let $cartoes = $('.cartao')
+    let cartoes = []
 
-  $cartoes.each(function () {
-    let cartao = {}
+    $cartoes.each(function () {
+      let cartao = {}
 
-    cartao.conteudo = $(this).find('.cartao-conteudo').text()
+      cartao.conteudo = $(this).find('.cartao-conteudo').text()
 
-    cartoes.push(cartao)
-  })
+      cartoes.push(cartao)
+    })
 
-  let mural = {
-    usuario : 'marco@gmail.com',
-    cartoes : cartoes
-  }
-
-  $sync.removeClass('botaoSync--sincronizado')
-  $sync.addClass('botaoSync--esperando')
-
-  $.ajax({
-    url : 'https://ceep.herokuapp.com/cartoes/salvar',
-    method : 'POST',
-    data : mural,
-    success : function (res) {
-      $sync.addClass('botaoSync--sincronizado')
-      console.log(`${res.quantidade} cartões salvos em ${res.usuario}`)
-    },
-    error : function () {
-      $sync.addClass('botaoSync--deuRuim')
-      console.log('Não foi possível salvar o mural')
-    },
-    complete : function () {
-      $sync.removeClass('botaoSync--esperando')
+    let mural = {
+      usuario : 'marco@gmail.com',
+      cartoes : cartoes
     }
+
+    $sync.removeClass('botaoSync--sincronizado')
+    $sync.addClass('botaoSync--esperando')
+
+    $.ajax({
+      url : 'https://ceep.herokuapp.com/cartoes/salvar',
+      method : 'POST',
+      data : mural,
+      success : function (res) {
+        $sync.addClass('botaoSync--sincronizado')
+        console.log(`${res.quantidade} cartões salvos em ${res.usuario}`)
+      },
+      error : function () {
+        $sync.addClass('botaoSync--deuRuim')
+        console.log('Não foi possível salvar o mural')
+      },
+      complete : function () {
+        $sync.removeClass('botaoSync--esperando')
+      }
+    })
   })
-})
+})();
